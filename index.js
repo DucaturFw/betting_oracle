6432bfa05ce38f98f763cba97298b1e8d8d18408
StarNeit <koneit@outlook.com>
2018-05-18 09:15:32 +0200

    remove mongodb connection

--- index.js
+++ index.js
@@ -13,17 +13,8 @@ dotenv.config();
 /**
  * Services
  */
-var sampleService = require("./src/services/sample");
+var bettingService = require("./src/services/sample");
 
-/**
- * Connect to MongoDB.
- */
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI, { useMongoClient: true });
-mongoose.connection.on("error", () => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running.");
-    process.exit();
-});
+const bettingDaemon = schedule.scheduleJob("*/" + process.env.SCHEDULE_INTERVAL + " * * * * *", bettingService);
 
-const sampleDaemon = schedule.scheduleJob("*/" + process.env.SCHEDULE_INTERVAL + " * * * * *", sampleService);
-
-console.log("---------- Started Sample Daemon ----------");
+console.log("---------- Started Betting Daemon ----------");
